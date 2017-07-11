c09d5d2d1c2b46789a9b10c06d45ac20709970f5
Jack Tan <Jack.Tan@airnz.co.nz>
2017-07-12 11:21:59 +1200

    Remove index.php

--- index.php
+++
@@ -1,3 +0,0 @@
-<!-- This php file is needed to trick Heroku to deploy static content. 
-Heroku will look for index.php, which then redirects you to index.html -->
-<?php header( 'Location: /index.html' ) ;  ?>
